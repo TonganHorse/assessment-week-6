01717685df437f2a7cfca62b5be9d2221e5169c2
tuli kinikini <tuli.kinikini@gmail.com>
2022-05-20 18:01:19 -0600

    added two tests for shufflearray

--- functions.test.ts
+++ functions.test.ts
@@ -3,9 +3,16 @@ const {shuffleArray} = require('./utils')
 describe('shuffleArray should', () => {
     test('return an array', () => {
         let array = [1,2,3]
-        expect(shuffleArray(array)).toEqual([...array])
+        expect(shuffleArray(array))
     }),
     test('returns same length as arg', () => {
-       expect
+        let array = [2,3,4,5,6]
+       expect(shuffleArray(array).length).toEqual(array.length)
+    }),
+    test('has same items', () => {
+        const expected = [1, 2, 3, 4, 5, 6];
+        expect(shuffleArray(expected))
+        .toEqual(expect.arrayContaining(expected));
     })
+
 })
